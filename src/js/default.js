8140066511565f8a2904aa3687e30c6e8a3c0590
Alistair McDonald <alistairmcdonald@Alistairs-MacBook-Pro.local>
2017-04-09 11:29:56 +0100

    Adds function to track link clicks in GA

--- src/js/default.js
+++ src/js/default.js
@@ -1,12 +1,23 @@
 (function () {
-	var emailLink = document.querySelector('.email');
+	var emailLink = document.getElementById("email");
 	emailLink.addEventListener('click', noSpam, false);
-	
+
+	var social = document.querySelector('.links');
+	social.addEventListener('click', gaEvent, false);
+
 	function noSpam(evt) {
+		console.log('noSpam');
 		evt.preventDefault();
 		var user = emailLink.dataset.localpart;
 		var domain = emailLink.dataset.domain;
 		var locationstring = "mailto:" + user + "@" + domain;
 		window.location = locationstring;
+	}
+
+	function gaEvent (evt) {
+		console.log('gaEvent');
+		evt.preventDefault();
+		var eventLabel = evt.target.id;
+		ga('send', 'event', 'link', 'click', eventLabel);
 	}
 }());
